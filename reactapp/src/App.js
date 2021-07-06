f3253b1c0129f3f99c6cfe130223cfe1c16c6dad
willem_Roy <ryllem@yahoo.fr>
2021-07-06 17:02:37 +0200

    browserList problem

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ScreenHome from './ScreenHome';
 import ScreenMyArticles from './ScreenMyArticles';
